10287258b038f72ce56ecdf760cb47cafe46dd63
Anupam Dahal <miracleanupam@gmail.com>
2020-12-26 23:36:24 +0200

    exercise 6.8; Anecdotes lists and anecdote moved to their separate components

---
+++ redux-anecdotes/src/components/Anecdotes.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { addVote } from '../reducers/anecdoteReducer'
+
+const Anecdote = ({ anecdote }) => {
+    const dispatch = useDispatch()
+
+    const vote = (id) => {
+        dispatch(addVote(id))
+    }
+
+    return (
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+    )
+}
+
+const Anecdotes = () => {
+    const anecdotes = useSelector(state => state)
+
+    return (
+        anecdotes.map(anecdote =>
+            <Anecdote key={anecdote.id} anecdote={anecdote} />
+        )
+    )
+}
+
+export default Anecdotes
