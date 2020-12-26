d46ed0cdc02cab464bac68bf47e3a04e2e161256
Anupam Dahal <miracleanupam@gmail.com>
2020-12-26 23:07:47 +0200

    exercise 6.6, 6.7; already done is previous commits

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -20,7 +20,6 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-  // console.log(state)
   switch (action.type) {
     case 'VOTE':
       const newState = state.map((a) => {
